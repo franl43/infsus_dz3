8e35971a59b195ac9642becddcf26703c0941c4c
fl53462 <fl53462@fer.hr>
2024-05-23 22:39:02 +0200

    spring init

---
+++ suhs/src/main/java/com/infsus/suhs/SuhsApplication.java
@@ -0,0 +1,13 @@
+package com.infsus.suhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhsApplication.class, args);
+	}
+
+}
---
+++ suhs/src/test/java/com/infsus/suhs/SuhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infsus.suhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
